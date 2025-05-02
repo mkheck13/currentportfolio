f7406be140fbd859e8725cb33fb723055a3f7927
mkheck13 <mkheck13@gmail.com>
2025-05-02 12:01:11 -0700

    fix

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import React from "react";
-import gitLogo from "../Assets/github-mark-white.png";
+import gitLogo from "../assets/github-mark-white.png";
 
 const FooterComponent = () => {
   return (
